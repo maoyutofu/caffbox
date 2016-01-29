73a942418d7bec6edceaba634d22bf3122678ee3
tjz101 <tjz101@qq.com>
2016-01-29 15:28:02 +0800

    add args

--- server/server.go
+++ server/server.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
 	. "github.com/tjz101/caffbox"
 	c "github.com/tjz101/caffbox/controller"
 	"github.com/tjz101/caffmux"
+	"os"
 )
 
+func command() {
+	arg_num := len(os.Args)
+	if arg_num > 1 {
+		for i := 1; i < arg_num; i++ {
+			cmd := os.Args[i]
+			if cmd == "-v" {
+				fmt.Println("caffbox version: caffbox/1.0.3")
+			}
+		}
+		os.Exit(0)
+	}
+}
+
 func main() {
+	command()
 	ParseConf()
 	caffmux.Debug("listen to " + Sett.Addr)
 
